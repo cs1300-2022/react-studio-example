c756f42794f0ded4ea200df45a6e399b3fa332d5
Dylan Hu <git@dylan.hu>
2022-10-20 19:01:17 -0400

    add Add to Cart button

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
 
   return (
     <div className="App flex flex-col items-center">
-      <div className="flex w-full md:max-w-[1024px]">
+      <div className="flex w-full sm:max-w-[1280px]">
         <main className="flex flex-col w-2/3 p-8 gap-8">
           <h1 className="text-4xl font-medium">Blueno's Bakery</h1>
           <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
@@ -26,13 +26,13 @@ function App() {
             ))}
           </div>
         </main>
-        <aside className="flex flex-col w-1/3 shrink-0 p-8 pt-24">
+        <aside className="flex flex-col w-1/3 shrink-0 p-8 sticky top-20 max-h-min">
           <h2 className="text-2xl font-medium">My Cart</h2>
           {Object.entries(cart).length === 0 ? (
             <p className="text-gray-500 mt-4">Nothing here just yet!</p>
           ) : (
             <>
-              <ul className="mt-4">
+              <ul className="mt-4 flex flex-col gap-4">
                 {Object.entries(cart).map(([index, quantity]) => {
                   const item = bakeryData[Number(index)];
                   return (
@@ -42,7 +42,7 @@ function App() {
                   );
                 })}
               </ul>
-              <p className="mt-4">
+              <p className="mt-4 font-medium">
                 Total: $
                 {Object.entries(cart).reduce((total, [index, quantity]) => {
                   const item = bakeryData[Number(index)];
--- src/components/BakeryItem.tsx
+++ src/components/BakeryItem.tsx
@@ -13,11 +13,8 @@ export default function BakeryItem({
   addToCart: () => void;
 }) {
   return (
-    <div
-      className="bg-gray-50 hover:bg-gray-100 rounded-md flex flex-col"
-      onClick={() => addToCart()}
-    >
-      <div className="flex-[1.5] w-full h-48 bg-gray-200 rounded-md overflow-hidden">
+    <div className="bg-gray-50 dark:bg-gray-600 hover:bg-gray-100 dark:hover:bg-gray-700 rounded-md flex flex-col">
+      <div className="flex-[2] md:flex-[1.5] w-full h-60 bg-gray-200 rounded-md overflow-hidden">
         <img
           src={item.image}
           alt={item.name}
@@ -27,9 +24,14 @@ export default function BakeryItem({
       <div className="flex-1 flex-grow p-4 flex flex-col justify-between">
         <div>
           <h2 className="text-xl font-bold">{item.name}</h2>
-          <p className="text-gray-500">{item.description}</p>
+          <p>{item.description}</p>
         </div>
-        <p className="text-gray-500">${item.price}</p>
+        <div className="flex items-center justify-between">
+          <p>${item.price}</p>
+          <button className="px-4 py-2 rounded-md" onClick={addToCart}>
+            Add to Cart
+          </button>
+        </div>
       </div>
     </div>
   );
